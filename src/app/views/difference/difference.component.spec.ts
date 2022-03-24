3e46478b73a6f4c28006a10ff482d2c875e84bd7
Paulo <pauloh2012sul@gmail.compwd>
2022-03-24 03:13:52 -0300

    ajuste progress bar

---
+++ src/app/views/difference/difference.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DifferenceComponent } from './difference.component';
+
+describe('DifferenceComponent', () => {
+  let component: DifferenceComponent;
+  let fixture: ComponentFixture<DifferenceComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DifferenceComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DifferenceComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
